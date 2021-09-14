fca88730941c4763bdaea9ff18275e5cd303e931
汪韬 <452663934@qq.com>
2021-09-14 17:28:35 +0800

    :tada: move dist

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
   title: 'kubecube front components',
-  base: '/dist',
+  base: '.',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
